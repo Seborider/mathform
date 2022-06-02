411996edbfbc0ca07bcc7e2b972ce98c6d3854d8
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 16:29:56 +0200

    ⚡️ Use directive instead of NgClass to change color of input

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { map } from 'rxjs/operators'
 
 @Directive({
   selector: '[appAnswerHighlight]'
@@ -8,4 +9,17 @@ export class AnswerHighlightDirective {
 
   constructor(private el: ElementRef, private controlName: NgControl) { }
 
+  ngOnInit() {
+    this.controlName.control?.parent?.valueChanges
+    .pipe(
+      map(({a, b, answer}) => Math.abs((a + b - answer) / (a + b)))
+    ).subscribe(value => {
+      if (value < 0.2) {
+        this.el.nativeElement.classList.add('close')
+      } else {
+        this.el.nativeElement.classList.remove('close')
+      }
+    })
+  }
+  
 }
