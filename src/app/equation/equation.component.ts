27b9188a4d55704b4f67b667925983ab3072a898
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 13:20:24 +0200

    🎨 Add subscription to statusChange

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -27,6 +27,18 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.mathForm.statusChanges.subscribe((value) => {
+      if (value === 'INVALID') {
+        return
+      }
+      
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ('')
+      })
+    }
+      )
   }
 
   randomNumber() {
