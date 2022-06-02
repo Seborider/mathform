9decab7221db0802a6d32cac5127e6996393316d
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 14:54:30 +0200

    ✨ Implement solution statistics

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -9,6 +9,7 @@ import { delay, filter } from 'rxjs';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
@@ -28,9 +29,17 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const startTime = new Date()
+    let numberSolved = 0
+
     this.mathForm.statusChanges.pipe(
       filter(value => value === "VALID"),
-      delay(100)).subscribe(() => {      
+      delay(100)).subscribe(() => {
+      numberSolved++,  
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000,
+        
       this.mathForm.setValue({
         a: this.randomNumber(),
         b: this.randomNumber(),
