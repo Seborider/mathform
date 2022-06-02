889631b180272a4911e24bd3cd108d7b69fbf555
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 11:19:34 +0200

    🎉 Setup component and reactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EquationComponent } from './equation/equation.component';
 
@@ -10,7 +10,8 @@ import { EquationComponent } from './equation/equation.component';
     EquationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
