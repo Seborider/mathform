50a5823ab8d47ccc407440a2ed80f9d8032b399d
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 11:46:02 +0200

    🎨 Add html markup and getter methods

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  })
 
   constructor() { }
+
+  get a() {
+    return this.mathForm.value.a
+  }
+
+  get b() {
+    return this.mathForm.value.b
+  }
 
   ngOnInit(): void {
   }
 
+  randomNumber() {
+    return Math.floor(Math.random() * 10)
+  }
 }
