a8ae8fb31e9b908d3ad933a0a658107b5efcd02e
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 12:47:39 +0200

    ♻️ Make validation class reusable

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -13,7 +13,7 @@ export class EquationComponent implements OnInit {
     b: new FormControl(this.randomNumber()),
     answer: new FormControl('')
   }, [
-    MathValidators.addition
+    MathValidators.addition('answer', 'a', 'b')
   ])
 
   constructor() { }
