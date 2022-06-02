943ca1c1f95776da4e49ac435f1d06c757d31801
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 11:16:29 +0200

    ✨ Add equation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EquationComponent } from './equation/equation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquationComponent
   ],
   imports: [
     BrowserModule
